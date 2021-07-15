ca4c8f3084de76f0fc97cefc8164cfd3a5c4c1c9
Dron2019 <mr.andrey@bigmir.net>
2021-07-15 09:50:12 +0300

    add single news page

--- src/assets/scripts/gulp-modules/single-news.js
+++ src/assets/scripts/gulp-modules/single-news.js
@@ -1,6 +1,6 @@
 /* eslint-disable no-new */
 // eslint-disable-next-line no-undef
-new Swiper('.mySwiper', {
+const swiper1 = new Swiper('.mySwiper', {
   slidesPerView: 1,
   spaceBetween: 0,
   loop: true,
@@ -9,5 +9,21 @@ new Swiper('.mySwiper', {
     nextEl: '.button-next',
     prevEl: '.button-prev',
   },
-
 });
+function setCurrentIndexView(index, elementArg, swiper) {
+  const element = elementArg;
+  const toView = index < 10 ? `0${index.toString()}` : index;
+  element.innerHTML = `
+    ${toView}
+    <div class="current">${swiper.slides.length - 2}</div>
+  `;
+}
+function changeNumbers(swiper) {
+  const current = document.querySelector('.all');
+  setCurrentIndexView((swiper.realIndex + 1), current, swiper);
+}
+
+changeNumbers(swiper1);
+
+swiper1.on('afterInit', changeNumbers);
+swiper1.on('activeIndexChange', changeNumbers);
