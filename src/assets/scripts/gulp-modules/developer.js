8a6a39d7b920286fe36d77afaf96e087e55b30fc
Dron2019 <mr.andrey@bigmir.net>
2021-07-16 12:45:35 +0300

    developer slider add and page adaptatin

--- src/assets/scripts/gulp-modules/developer.js
+++ src/assets/scripts/gulp-modules/developer.js
@@ -4,8 +4,29 @@ const swiper = new Swiper('.mySwiper', {
   slidesPerView: 6,
   spaceBetween: 56,
   autoplay: true,
+  loop: true,
   pagination: {
     el: '.swiper-pagination',
     clickable: true,
+  },
+  breakpoints: {
+    // when window width is >= 320px
+    320: {
+      slidesPerView: 1,
+    },
+    350: {
+      slidesPerView: 2,
+    },
+    // when window width is >= 480px
+    576: {
+      slidesPerView: 3,
+    },
+    // when window width is >= 640px
+    950: {
+      slidesPerView: 4,
+    },
+    1100: {
+      slidesPerView: 6,
+    },
   },
 });
