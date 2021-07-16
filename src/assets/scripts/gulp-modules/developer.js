7158e18b6ec5f8201244acb4a732bb837de9b9d3
Dron2019 <mr.andrey@bigmir.net>
2021-07-16 11:53:43 +0300

    slider developers add

---
+++ src/assets/scripts/gulp-modules/developer.js
@@ -0,0 +1,11 @@
+/* eslint-disable no-undef */
+/* eslint-disable no-unused-vars */
+const swiper = new Swiper('.mySwiper', {
+  slidesPerView: 6,
+  spaceBetween: 56,
+  autoplay: true,
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+});
