02e0a0ca2062cd15f279c823c518ad47a66ff12c
Dron2019 <mr.andrey@bigmir.net>
2021-08-04 15:03:18 +0300

    add build new popup

--- src/assets/scripts/gulp-modules/single-build-progress.js
+++ src/assets/scripts/gulp-modules/single-build-progress.js
@@ -167,3 +167,162 @@ webCamClose.addEventListener('click', (evt) => {
     .set(webCameraPopup.querySelector('.web-camera-popup'), { autoAlpha: 0, clearProps: 'all' })
     .set(webCameraPopup, { autoAlpha: 0, y: 0, clearProps: 'all' });
 });
+
+
+class Popup {
+  constructor(props) {
+    this.call = props.call;
+    this.overlayClass = 'my-popup-overlay';
+    this.uniqueClass = `${this.overlayClass}-${Math.random().toString().replace('.', '')}`;
+    this.styles = props.styles;
+    this.content = props.content;
+    this.close = props.close;
+    this.afterOpenCb = props.afterOpenCb || function () {};
+    this.init();
+  }
+
+  init() {
+    document.body.insertAdjacentHTML('beforeend', this.preparePopup());
+    this.mountedPopup = document.querySelector(`.${this.uniqueClass}`);
+    Object.entries(this.styles).forEach((el) => {
+      // eslint-disable-next-line prefer-destructuring
+      this.mountedPopup.style[el[0]] = el[1];
+    });
+    this.addContent();
+    this.handleCallButton();
+    this.mountedPopup.addEventListener('click', (evt) => {
+      if (evt.target.classList.contains(this.uniqueClass)) {
+        this.closePopup();
+      }
+    });
+    this.close.addEventListener('click', () => {
+      this.closePopup();
+    });
+    this.afterOpenCb();
+  }
+
+  handleCallButton() {
+    // eslint-disable-next-line no-prototype-builtins
+    if (NodeList.prototype.isPrototypeOf(this.call)) {
+      this.call.forEach((button) => {
+        button.addEventListener('click', () => {
+          this.openPopup();
+        });
+      });
+    } else {
+      this.call.addEventListener('click', () => {
+        this.openPopup();
+      });
+    }
+  }
+
+  // handleOpeningButtons
+  addContent() {
+    this.mountedPopup
+      .querySelector('.my-popup-content')
+      .insertAdjacentElement('beforeend', this.content);
+  }
+
+  closePopup() {
+    gsap.timeline()
+      .timeScale(2)
+      .fromTo(
+        this.mountedPopup.querySelector('.my-popup-content'),
+        { y: 0, autoAlpha: 1 },
+        { y: -100, autoAlpha: 0 },
+      )
+      .fromTo(
+        this.mountedPopup,
+        { autoAlpha: 1 },
+        { autoAlpha: 0 },
+      )
+      .set(this.mountedPopup, { display: 'none' });
+  }
+
+  openPopup() {
+    gsap.timeline()
+      .timeScale(1.5)
+      .set(this.mountedPopup, { display: 'flex' })
+      .fromTo(
+        this.mountedPopup,
+        { autoAlpha: 0 },
+        { autoAlpha: 1 },
+      )
+      .fromTo(
+        this.mountedPopup.querySelector('.my-popup-content'),
+        { y: -100, autoAlpha: 0 },
+        { y: 0, autoAlpha: 1 },
+      )
+      .add(this.afterOpenCb);
+  }
+
+  get popupStyles() {
+    return `
+      <style>
+        .my-popup-overlay {
+          position:fixed;
+          display: none;
+          left:0;
+          top:0;
+          width:100vw;
+          height: 100vh;
+          justify-content:center;
+          align-items: center;
+          z-index:1000;
+        }
+      </style>
+    `;
+  }
+
+  preparePopup() {
+    return `
+      <div class="my-popup-overlay ${this.uniqueClass}">
+        <div class="my-popup-content">
+        </div>
+      </div>
+      ${this.popupStyles}
+    `;
+  }
+}
+
+
+const navSwiper = new Swiper('[data-build-slides-nav]', {
+  slidesPerView: 6,
+  spaceBetween: 30,
+  slideToClickedSlide: true,
+});
+const popupSwiper = new Swiper('[data-build-popup-slides]', {
+  // effect: 'fade',
+  loop: true,
+  speed: 1200,
+  navigation: {
+    nextEl: '.detailed-nav-next',
+    prevEl: '.detailed-nav-prev',
+  },
+  thumbs: {
+    swiper: navSwiper,
+  },
+});
+
+popupSwiper.update();
+navSwiper.update();
+
+
+new Popup({
+  call: document.querySelectorAll('[data-call-build-popup]'),
+  close: document.querySelector('[data-detailed-popup-close]'),
+  styles: {
+    backgroundColor: 'rgba(56, 62, 65, 0.99)',
+  },
+  afterOpenCb: () => {
+    popupSwiper.update();
+    navSwiper.update();
+  },
+  content: document.querySelector('.detailed-photos-popup'),
+});
+
+document.querySelectorAll('[data-call-build-popup]').forEach((button, index) => {
+  button.addEventListener('click', () => {
+    popupSwiper.slideTo(index);
+  });
+});
